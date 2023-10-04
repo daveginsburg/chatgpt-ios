e669a9e857d8f3f6def1cd4eb57faf6e06c4e711
daveginsburg <gins@thirasystems.com>
2023-10-04 13:51:03 -0700

    Add files via upload

---
+++ test_map_projectApp.swift
@@ -0,0 +1,19 @@
+//
+//  test_map_projectApp.swift
+//  test map project
+//
+//  Created by David Ginsburg on 7/27/23.
+//
+
+import SwiftUI
+
+@main
+struct test_map_projectApp: App {
+ 
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+           }
+    }
+}
+
